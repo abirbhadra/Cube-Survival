fdeae80aec55ba43db14c935db38521aad6c54b8
abirbhadra <58373108+abirbhadra@users.noreply.github.com>
2023-12-07 05:43:40 +0530

    polish

--- Assets/Scripts/MenuUIScript.cs
+++ Assets/Scripts/MenuUIScript.cs
@@ -8,15 +8,50 @@ using UnityEngine.UI;
 public class MenuUIScript : MonoBehaviour
 {
     public Button MenuPlay;
+    public Button MenuExit;
+    public Button ExitUICross;
+    public GameObject ExitUI;
+    public GameObject MenuUI;
 
     private void Start()
     {
         MenuPlay.onClick.AddListener(PlayButton);
+        MenuExit.onClick.AddListener(ExitButton);
+        ExitUICross.onClick.AddListener(ExitUICrossButton);
+    }
+
+    private void EnableMenuUI()
+    {
+        MenuUI.SetActive(true);
+    }
+    private void DisableMenuUI()
+    {
+        MenuUI.SetActive(false);
+    }
+    private void EnableExitUI()
+    {
+        ExitUI.SetActive(true);
+    }
+    private void DisableExitUI()
+    {
+        ExitUI.SetActive(false);
     }
 
     private void PlayButton()
     {
         Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+    private void ExitButton()
+    {
+        Time.timeScale = 1;
+        EnableExitUI();
+        DisableMenuUI();
+    }
+    private void ExitUICrossButton()
+    {
+        Time.timeScale = 1;
+        DisableExitUI();
+        EnableMenuUI();
     }
 }
